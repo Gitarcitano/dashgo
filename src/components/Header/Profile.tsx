967914e6c1b9895aaf9fb00f4624abd139f88110
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2021-05-03 19:38:14 -0300

    refactor: responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+  export function Profile({ showProfileData }: ProfileProps ) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Giovanne Tarcitano</Text>
-        <Text color="gray.300" fontSize="sm">
-          giovanne.tarcitano@gmail.com
-        </Text>
-      </Box>
-
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Giovanne Tarcitano</Text>
+          <Text color="gray.300" fontSize="sm">
+            giovanne.tarcitano@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Giovanne Tarcitano" src="https://github.com/gitarcitano.png"/>
     </Flex>
   );
